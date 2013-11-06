c34f90eb8cf36e8962a6c1f352984c0e956044d4
Georgy Ivanov <ivanov.georg@gmail.com>
2013-11-06 19:13:34 +0400

    counting_sort for arbitrary number

--- counting_sort.cpp
+++ counting_sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cstdio>
+#include <cstring>
 
 int get_digit(const int &data) {
   return data % 10;
@@ -18,14 +19,36 @@ void counting_sort(int *a, int n, int(&_get_digit)(const int &)) {
 
   for (int i = 0; i < n; ++i) {
     ++cnt[_get_digit(a[i])];
-  }
+  } 
 
+  // {1,1,1,0,2} -> {1,2,3,3,5}
+  // {1,1,1,0,2} -> {0,1,2,3,3}
+  int sum = 0;
+  for (int i = 0; i < abc_size; ++i) {
+    int tmp = cnt[i];
+    cnt[i] = sum;
+    sum += tmp;
+  }
+/*
   int j = 0;
   for (int i = 0; i < abc_size; ++i) {
     for (int k = 0; k < cnt[i]; ++k, ++j) {
-      a[j] = i;
+//      a[j] = i;
+      res[j] = ... a[l];        
     }
   }
+  */
+  int *res = new int[n];
+  for (int i = 0; i < n; ++i) {
+    res[cnt[_get_digit(a[i])]] = a[i];
+    ++cnt[_get_digit(a[i])];
+  }
+  
+  memcpy(a, res, n*sizeof(int));
+  
+  delete [] res;
+  delete [] cnt;
+
 }
 
 
